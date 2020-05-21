8412d9cfd86a1f548dbb902ce35e5a74b7dfdb7b
FCK <2503199135@qq.com>
2020-05-21 15:04:30 +0800

    commit

---
+++ src/main/java/com/example/demo/common/RespBean.java
@@ -0,0 +1,44 @@
+package com.example.demo.common;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 接口统一返回的bean
+ */
+@Data
+@ToString
+public class RespBean {
+    private Integer status;
+    private String msg;
+    private Object data;
+
+    private RespBean() {
+    }
+
+    public static RespBean build() {
+        return new RespBean();
+    }
+
+    public static RespBean ok(String msg, Object data) {
+        return new RespBean(200, msg, data);
+    }
+
+    public static RespBean ok(String msg) {
+        return new RespBean(200, msg, null);
+    }
+
+    public static RespBean error(String msg, Object data) {
+        return new RespBean(500, msg, data);
+    }
+
+    public static RespBean error(String msg) {
+        return new RespBean(500, msg, null);
+    }
+
+    private RespBean(Integer status, String msg, Object data) {
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+}
