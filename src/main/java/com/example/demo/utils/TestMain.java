89df608c414e1cfb97d8ea097ce9f43021615a6a
FCK <2503199135@qq.com>
2020-06-12 15:34:11 +0800

    commit

--- src/main/java/com/example/demo/utils/TestMain.java
+++ src/main/java/com/example/demo/utils/TestMain.java
@@ -1,5 +1,6 @@
 package com.example.demo.utils;
 
+import chrriis.common.UIUtils;
 import chrriis.dj.nativeswing.swtimpl.NativeComponent;
 import chrriis.dj.nativeswing.swtimpl.NativeInterface;
 import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
@@ -8,10 +9,13 @@ import chrriis.dj.nativeswing.swtimpl.components.WebBrowserEvent;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
+import static sun.plugin.javascript.navig.JSType.URL;
 
 /**
  * @author fan
@@ -103,15 +107,15 @@ public class TestMain extends JPanel {
                                                          //截图部分图形
                                                          image = image.getSubimage(0, 0, maxWidth, maxHeight);
                         /*此部分为使用缩略图
-                        int width = image.getWidth(), height = image
-                            .getHeight();
-                         AffineTransform tx = new AffineTransform();
-                        tx.scale((double) maxWidth / width, (double) maxHeight
-                                / height);
-                        AffineTransformOp op = new AffineTransformOp(tx,
-                                AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-                        //缩小
-                        image = op.filter(image, null);*/
+                                                         int width = image.getWidth(), height = image
+                                                                 .getHeight();
+                                                         AffineTransform tx = new AffineTransform();
+                                                         tx.scale((double) maxWidth / width, (double) maxHeight
+                                                                 / height);
+                                                         AffineTransformOp op = new AffineTransformOp(tx,
+                                                                 AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+                                                         //缩小
+                                                         image = op.filter(image, null);*/
                                                      }
                                                      try {
                                                          // 输出图像
@@ -119,7 +123,6 @@ public class TestMain extends JPanel {
                                                      } catch (IOException ex) {
                                                          ex.printStackTrace();
                                                      }
-                                                     // 退出操作
                                                      System.exit(0);
                                                  }
                                              }
@@ -129,15 +132,33 @@ public class TestMain extends JPanel {
     }
 
     public static void main(String[] args) {
+        /*final String url = "<html><head></head><body><div style=\"position:relative;left:0;top:0;width:212px;height:110px\"><div style=\"position: absolute;left:0px;top:0px;width:212px;height:110px;z-index:1\"><div style=\"position:relative;width:212px;height:110px\"><div style=\"position:absolute;white-space:nowrap;overflow:hidden;color:rgba(255,0,0,1);location:center;background-color:rgba(255,255,255,0);width:212px;top:0px;left:0px;z-index:2;font-size:32px;font-family:Microsoft Yahei;letter-spacing:0px;\">碳烧咖啡22</div><div style=\"position:absolute;word-wrap:nowrap;word-break:break-all;overflow:hidden;color:rgba(0,0,0,1);location:center;background-color:rgba(255,255,255,0);width:50px;top:46px;left:69px;z-index:2;font-size:15px;font-family:SimSun;letter-spacing:0px;\">罐装1</div><div style=\"position:absolute;word-wrap:nowrap;word-break:break-all;overflow:hidden;color:rgba(0,0,0,1);location:center;background-color:rgba(255,255,255,0);width:50px;top:46px;left:114px;z-index:2;font-size:15px;font-family:SimSun;letter-spacing:0px;\">罐装1</div><div style=\"position:absolute;display: flex;justify-content: center;align-items: center;width:50px;height:50px;top:56px;left:0px;z-index:2;background-color:rgba(255,255,255,0);image-type:100;\"><img style=\"max-width:100%;max-height:100%\" src=\"http://10.6.3.81:8090/ishelf/imgs/upload/product/null/2020/6/11/1528444040.png\"></img></div><div style=\"position:absolute;word-wrap:nowrap;word-break:break-all;overflow:hidden;color:rgba(0,0,0,1);location:center;background-color:rgba(255,255,255,0);width:50px;top:69px;left:69px;z-index:2;font-size:15px;font-family:SimSun;letter-spacing:0px;\">好喝11</div></div></div><br/></div></body></html>";
+        final String title = "DJ Test";
+        UIUtils.setPreferredLookAndFeel();
+        NativeInterface.open();
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame(title);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.getContentPane().add(new TestMain(url), BorderLayout.CENTER);
+                frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                frame.setLocationByPlatform(true);
+                frame.setVisible(true);
+            }
+        });
+        NativeInterface.runEventPump();*/
+
         NativeInterface.open();
         SwingUtilities.invokeLater(() -> {
             // SWT组件转Swing组件，不初始化父窗体将无法启动webBrowser
             JFrame frame = new JFrame("以DJ组件保存指定网页截图");
             // 加载指定页面，最大保存为640x480的截图
             frame.getContentPane().add(
-                    new TestMain("https://www.baidu.com/", 1920, 1080),
+                    new TestMain("http://116.62.200.160:8090/iknow/login", 1920, 1080),
                     BorderLayout.CENTER);
-            frame.setSize(1024, 768);
+            frame.setSize(1920, 1080);
             // 仅初始化，但不显示
             frame.invalidate();
             frame.pack();
@@ -145,4 +166,18 @@ public class TestMain extends JPanel {
         });
         NativeInterface.runEventPump();
     }
+
+/*    public TestMain(final String html) {
+        super(new BorderLayout());
+        JPanel webBrowserPanel = new JPanel(new BorderLayout());
+        JWebBrowser webBrowser = new JWebBrowser();
+        webBrowser.setHTMLContent(html);
+        webBrowser.setButtonBarVisible(false);
+        webBrowser.setMenuBarVisible(false);
+        webBrowser.setBarsVisible(false);
+        webBrowser.setStatusBarVisible(false);
+        webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
+        add(webBrowserPanel, BorderLayout.CENTER);
+    }*/
+
 }
